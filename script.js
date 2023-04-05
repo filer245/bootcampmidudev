4c90ad91692ff5f2a110fd83ebad076964779102
filer245 <sergiopaniagua2006@gmail.com>
2023-04-05 23:10:19 +0200

    InicioReact

---
+++ script.js
@@ -0,0 +1,15 @@
+const persona = {
+    firstName: 'Sergio',
+    lastName: 'Paniagua',
+    age: 16,
+    gender: 'Macho',
+    isDeveloper: true,
+    twitter: '@hacienda_boy',
+    parents: ['Feli', 'Jose Manuel']
+}
+
+const sumar = (a, b) => {
+    console.log(a);
+    console.log(b);
+    return a+b;
+}
