b12f9db13e482977eb9eb2c5272aa765541ea17e
filer245 <sergiopaniagua2006@gmail.com>
2023-04-07 17:56:29 +0200

    Ej 1.1

--- part2/ejercicios3/src/index.js
+++ part2/ejercicios3/src/index.js
@@ -7,6 +7,10 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [mostrar, setMostrar] = useState(false)
+  let total = (good + neutral + bad);
+  let promedio = (((1*good) + (-1*bad))/total)
+  let positivos = ((good*100)/total)
 
   const clickG = () => {
     setGood(good + 1);
@@ -24,6 +28,11 @@ const App = () => {
     setGood(0);
     setNeutral(0);
     setBad(0);
+    setMostrar(false)
+  }
+
+  const mostrarEst = () => {
+    setMostrar(true)
   }
 
   return (
@@ -36,7 +45,12 @@ const App = () => {
       <p>Buenas: {good}</p>
       <p>Neutrales: {neutral}</p>
       <p>Malas: {bad}</p>
-      <small>Resetear: </small>
+      <h3>Stadísticas</h3>
+      <p>Total: {total}</p>
+      <p>Promedio(1 b, 0 n, -1 m): {mostrar ? promedio.toFixed(2) : 0}</p>
+      <p>% de positivos: {mostrar ? positivos.toFixed(2) : 0}</p>
+      <button onClick={mostrarEst}>Crear estadísticas</button>
+      <p>Resetear: </p>
       <button onClick={clickR}>Reset</button>
     </div>
   )
