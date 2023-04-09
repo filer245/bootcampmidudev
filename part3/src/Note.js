c423b3dfc81cbffe86fb88e4241a5b5bc9e46de5
filer245 <sergiopaniagua2006@gmail.com>
2023-04-09 18:44:29 +0200

    part3

---
+++ part3/src/Note.js
@@ -0,0 +1,14 @@
+export const Note = (props) => {
+  const {content, date} = props;
+
+  return (
+    <li>
+      <p>{content}</p>
+      <small>
+          <time>
+              {date}
+          </time>
+      </small>
+    </li>
+  )
+}
